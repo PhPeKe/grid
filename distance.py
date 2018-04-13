786038df07f19787ab3dde4fa7cd4a30c118ebb1
Phillip <p.kersten@live.de>
2018-04-13 12:21:16 +0200

    minor changes

--- distance.py
+++ distance.py
@@ -1,16 +1,22 @@
-houses = [{ "location" : (1,2), "distance" : [], "consumption" : 30},
-          { "location" : (1,2), "distance" : [], "consumption" : 29},
-          { "location" : (9,5), "distance" : [], "consumption" : 11},
-          { "location" : (8,3), "distance" : [], "consumption" : 28}]
-battery = (3,2)
-distances = []
+import csv
+import matplot
 
-def manhattan(houses,battery):
-    for house in houses:
+with open ("data/huizen1.csv","r") as f:
+    reader = csv.reader(f)
+    houses = map(tuple, reader)
+
+test_houses = [{ "location" : (1,2), "consumption" : 30},
+               { "location" : (1,2), "consumption" : 29},
+               { "location" : (9,5), "consumption" : 11},
+               { "location" : (8,3), "consumption" : 28}]
+
+battery = (3,2)
+
+def manhattan(test_houses,battery):
+    for house in test_houses:
         distance = 0
         for h,b in house["location"], battery:
             distance += abs(h - b)
-
         house["distance"] = distance
 
-manhattan(houses, battery)
+manhattan(test_houses, battery)
