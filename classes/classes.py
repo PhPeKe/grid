dd230bdb45b56356b9b3af600a96a5ed2f35a06b
Phillip <p.kersten@live.de>
2018-04-17 20:36:49 +0200

    tried to implement distance, still buggy: only selects battery 0

--- classes/classes.py
+++ classes/classes.py
@@ -4,18 +4,31 @@ class House:
         self.location = (x,y)
         self.output = output
         self.id = id
-        self.distance = set()
+        self.distance = 1000
         self.connection = set()
 
-    def manhattan(self, battery):
-        distance = 0
-        for h,b in self.location, battery.location:
-            distance += abs(h-b)
-        self.distance = distance
+    def manhattan(self, batteries):
+        for battery in batteries:
+            distance = 0
+            for h,b in self.location, battery.location:
+                print(h,b)
+                distance += abs(h-b)
+            print("distance battery ", battery.id, distance)
+            if distance < self.distance:
+                self.distance = distance
+                self.connection = battery
 
 class Battery:
 
-    def __init__(self, x, y, output,id):
+    def __init__(self, x, y, capacity,id):
         self.location = (x,y)
-        self.capacity = output
+        self.capacity = capacity
         self.id = id
+
+class Cable:
+
+    def __init__(self):
+        self.costs = set()
+        self.connection_battery = set()
+        self.connection_house = set()
+        self.edge = set()
