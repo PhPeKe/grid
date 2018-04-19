3b73e8b0afbea3b6ad095247307390ec16ef5a85
Phillip <p.kersten@live.de>
2018-04-19 16:34:03 +0200

    Not sure what changed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from functions.loadData import loadData
 from functions.calculateCosts import calculateCosts
 
 # Specify paths for data to load
-housePath = "data/wijk1_huizen.csv"
-batteryPath = "data/wijk1_batterijen.txt"
+housePath = "data/wijk2_huizen.csv"
+batteryPath = "data/wijk2_batterijen.txt"
 
 # Load in data
 houses, batteries = loadData(housePath, batteryPath)
