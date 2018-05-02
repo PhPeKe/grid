2810a60dda65f55798aa0d842d6ab39c4e862803
niiinja <nina@nina.ninja>
2018-05-02 08:09:50 +0200

    better prevent disconnect solution

--- functions/hillclimber.py
+++ functions/hillclimber.py
@@ -1,27 +1,24 @@
 def hillclimber(house, batteries):
 
-    #battery = house.possible_connections[0][0]
-
     batteries.sort(key=lambda x: x.capacity, reverse=True)
-    battery = batteries[0]
+    for battery in batteries:
+        capacity_d = house.output - battery.capacity
 
-    capacity_d = house.output - battery.capacity
+        # Find a house which is already connected
+        for connectedHouse in battery.connectedHouses:
+            # then check if the house's output combined with its battery's leftover capacity could facilitate the other
+            if (connectedHouse.output + connectedHouse.connection.capacity) <= capacity_d:
 
-    # Find a house which is already connected
-    for connectedHouse in battery.connectedHouses:
-        # then check if the house's output combined with its battery's leftover capacity could facilitate the other
-        if (connectedHouse.output + connectedHouse.connection.capacity) <= capacity_d:
+                for b in connectedHouse.possible_connections:
+                    if b[0].capacity >= connectedHouse.output:
+                        oldconnection = connectedHouse.connection
+                        connectedHouse.connection = b[0]
+                        house.connection = oldconnection
+                        print("SWITCH of house ", house.id, " and house ", connectedHouse.id)
+                        return
 
-            for b in connectedHouse.possible_connections:
-                if b[0].capacity >= connectedHouse.output:
-                    oldconnection = connectedHouse.connection
-                    connectedHouse.connection = b[0]
-                    house.connection = oldconnection
-                    print("SWITCH of house ", house.id, " and house ", connectedHouse.id)
-                    return
-        else:
-            print("no possible switches found for house: ", house)
-            battery
+    print("no possible switches found for house: ", house)
+
 
 # 1.voor alle huizen die niet eerste keuze batterij hebben:
 # 2.	hoeveel capaciteit moet er bij de eerste keus batterij worden vrijgemaakt?
