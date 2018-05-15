3bf06bbdc6506bbeb5e284e8e6767ef21478781f
Phillip <p.kersten@live.de>
2018-05-15 13:17:09 +0200

    Removed import for BnB

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from functions.calculateCosts import calculateCosts
 from functions.visualize import visualize
 from functions.prompt import prompt
 from functions.switch import switch
-from functions.BnB import optimize
 import sys
 from random import shuffle
 
