8995b627708228f27d923f1e2560c72ddb376f7b
Phillip <p.kersten@live.de>
2018-05-21 12:59:58 +0200

    Added flags again

---
+++ functions/helpers/arguments.py
@@ -0,0 +1,33 @@
+import argparse
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--district",
+                        help = "Specify the number of the district (1, 2, 3)",
+                        choices = ["1","2","3"])
+    parser.add_argument("-p","--plot",
+                        help = "Flag if plots should be made",
+                        action = "store_true")
+    parser.add_argument("-s","--sort",
+                        help = "Sort houses before connecting to specify which house should be connected first (ascending, descending, random)",
+                        choices = ["ascending","descending","random"])
+    parser.add_argument("-m","--method",
+                        help = "Specify the method of initially assigning houses to batteries (greedy, random)",
+                        choices = ["greedy","random"])
+    parser.add_argument("-sv","--save",
+                        help = "Specify if and how districts should be saved",
+                        choices = ["csv","verbose"])
+    args = parser.parse_args()
+
+    # Force valid input of required arguments
+    if args.district == None:
+        args.district = input("Choose district: ")
+        while args.district not in ["1","2","3"]:
+            args.district = input("Choose district(1, 2 or 3): ")
+
+    if args.method == None:
+        args.method = input("Choose method: ")
+        while args.method not in ["greedy","random"]:
+            args.method = input("Choose method(\"greedy\" or \"random\"): ")
+
+    return args
